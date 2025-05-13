7fe4ee148f92ca497f2519224c69d3e7d5dbc5c0
mehedi-66 <19303001@iubat.edu>
2025-05-14 02:31:53 +0600

    React UI start

--- includes/rest-api.php
+++ includes/rest-api.php
@@ -1,4 +1,21 @@
 <?php
+
+add_action('rest_api_init', function () {
+    
+    register_rest_route('custom/v1', '/quizzes', [
+        'methods' => 'GET',
+        'callback' => 'get_basic_quizzes',
+        'permission_callback' => '__return_true',
+    ]);
+
+    register_rest_route('custom/v1', '/quiz/(?P<id>\d+)', [
+        'methods' => 'GET',
+        'callback' => 'get_quiz_by_id',
+        'permission_callback' => '__return_true',
+    ]);
+});
+
+
 function expose_quiz_questions_meta_to_rest() {
     register_post_meta('quiz', '_quiz_questions', [
         'type'         => 'array',
@@ -23,3 +40,36 @@ function expose_quiz_questions_meta_to_rest() {
 }
 add_action('init', 'expose_quiz_questions_meta_to_rest');
 
+function get_basic_quizzes() {
+    $quizzes = get_posts([
+        'post_type' => 'quiz',
+        'numberposts' => -1,
+        'post_status' => 'publish',
+    ]);
+
+    return array_map(function ($quiz) {
+        return [
+            'id' => $quiz->ID,
+            'title' => get_the_title($quiz),
+            'slug' => $quiz->post_name,
+            'link' => get_permalink($quiz),
+        ];
+    }, $quizzes);
+}
+
+function get_quiz_by_id($data) {
+    $quiz_id = $data['id'];
+    $post = get_post($quiz_id);
+
+    if (!$post || $post->post_type !== 'quiz') {
+        return new WP_Error('not_found', 'Quiz not found', ['status' => 404]);
+    }
+
+    return [
+        'id' => $post->ID,
+        'title' => get_the_title($post),
+        'slug' => $post->post_name,
+        'link' => get_permalink($post),
+        'questions' => get_post_meta($post->ID, '_quiz_questions', true),
+    ];
+}
